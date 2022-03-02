c5f75def0ef97d8cb0ac9a0396c9073b7b898efb
Matthew Fisher <mwfisher91@gmail.com>
2022-03-02 13:09:11 -0800

    functions ex.

---
+++ exercise/functions/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+
+    println!("Area is {}", area_of(width, height));
+    println!("Volume is {}", volume_of(width, height, depth));
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn volume_of(x: i32, y: i32, z: i32) -> i32 {
+    x * y * z
+}
